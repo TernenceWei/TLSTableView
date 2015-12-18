b1b53b4cc06117181cbe617857f1a91a43005ffd
TernenceWei <ternence@leomaster.com>
2015-12-18 13:53:07 +0800

    Initial Commit

---
+++ TLSTableView/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  TLSTableView
+//
+//  Created by Ternence on 15/12/18.
+//  Copyright © 2015年 Leomaster. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
